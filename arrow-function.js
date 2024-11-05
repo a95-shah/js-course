f486cdc3a0e38d221a9f21d5a8adbb9387ef0dd9
shah g <shahahsan235@gmail.com>
2024-11-05 11:07:45 +0500

    added

--- arrow-function.js
+++ arrow-function.js
@@ -17,6 +17,36 @@
 
 // basic arrow function
 
+// Arrow function with two parameters 
+// const sum = (firstParam, secondParam) => { 
+//     return firstParam + secondParam; 
+//   }; 
+//   console.log(sum(2,5)); // Prints: 7
+
+
+// const addition =(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addition(2,4));
+
+// teronary operator
+let price = 10.5;
+let day = "tuesday";
+
+day === "Monday" ? price -= 1.5 : price += 1.5;
+// console.log(day,price);
+
+
+const weeksInYear = Math.floor(365/7);
+
+// calcuates the number of days left over after 365 is divded by 7
+const daysLeftOver = 365 % 7 ;
+
+// console.log("A year has " + weeksInYear + " weeks and " + daysLeftOver + " days");
+
+
+
+
 // const addnumber=(num1,num2)=>{
     // return num1+num2
 // }
@@ -53,11 +83,8 @@
 
 // Arrow functions introduce concise body syntax, or implicit return. This allows the omission of the curly brackets and the return keyword. Implicit return is useful for creating succinct one-line operations in map , filter , and other common array methods
 
-// const myNum = [1,2,3,4,5,6,7,8,9]
-// const newNum = myNum.filter((num)=>{
-//     return num>4
-// })
+const myNum = [1,2,3,4,5,6,7,8,9]
+const newNum = myNum.filter((num)=>{
+    return num>4
+})
 // console.log(newNum);
-
-
-
